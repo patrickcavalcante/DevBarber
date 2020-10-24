50bf86254e3c76c6111e7e91bf0a9a7073787d2f
Patrick Cavalcante <patrickc.lima2@gmail.com>
2020-10-24 12:40:12 -0300

    construcao TabBar

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,14 +1,12 @@
-import React, { useEffect , useContext } from 'react';
-import { StatusBar } from 'react-native';
-import { Container , LoadingIcon } from './styles';
+import React, { useEffect, useContext } from 'react';
+import { Container, LoadingIcon } from './styles';
 import AsyncStorage from '@react-native-community/async-storage';
 import { useNavigation } from '@react-navigation/native';
-import {UserContext} from '../../contexts/UserContext';
+
+import { UserContext } from '../../contexts/UserContext';
+import Api from '../../Api';
 
 import BarberLogo from '../../assets/barber.svg';
-
-
-import Api from '../../Api';
 
 export default () => {
 
@@ -18,27 +16,28 @@ export default () => {
     useEffect(()=>{
         const checkToken = async () => {
             const token = await AsyncStorage.getItem('token');
-            if(token){
+            if(token) {
                 let res = await Api.checkToken(token);
                 if(res.token) {
-                
-                    await AsyncStorage.setItem('token', res.token);
-                    userDispatch({
-                       type: 'setAvatar',
-                       payload:{
-                           avatar: json.data.avatar
-                       }
-                    });
-        
-                    navigation.reset({
-                        routes:[{name: 'MainTab'}]
-                    })
 
-                }else{
-                   navigation.navigate('SignIn'); 
+                    await AsyncStorage.setItem('token', res.token);
+
+                    userDispatch({
+                        type: 'setAvatar',
+                        payload:{
+                            avatar: res.data.avatar
+                        }
+                    });
+
+                    navigation.reset({
+                        routes:[{name:'MainTab'}]
+                    });
+
+                } else {
+                    navigation.navigate('SignIn');
                 }
-            }else{
-                   navigation.navigate('SignIn');
+            } else {
+                navigation.navigate('SignIn');
             }
         }
         checkToken();
@@ -46,9 +45,8 @@ export default () => {
 
     return (
         <Container>
-            <StatusBar barStyle={"light-content"} backgroundColor="#63C2D1"/>
-            <BarberLogo width="100%" height="160"/>
-            <LoadingIcon size="large" color="#FFFFFF"/>
+            <BarberLogo width="100%" height="160" />
+            <LoadingIcon size="large" color="#FFFFFF" />
         </Container>
     );
 }
